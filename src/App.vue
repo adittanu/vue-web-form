f7d0cae404deeeda7715894a6814274cbbf99281
Adit Tanu <adiettanu27@gmail.com>
2022-12-22 12:30:06 +0700

    start form

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <SignupForm />
+</template>
+
+<script>
+import SignupForm from "./components/SignupForm.vue";
+export default {
+  name: "App",
+  components: { SignupForm },
+};
+</script>
+
+<style>
+#app {
+  font-family: Arial, Helvetica, sans-serif;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+body {
+  margin: 0;
+  background: #eee;
+}
+</style>
